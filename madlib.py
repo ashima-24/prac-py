c51ba3ab326c1f7748c40d17f986bd83d6cac0d9
ashima-24 <jindal241995@gmail.com>
2019-01-29 00:56:10 +0100

    madlib

---
+++ madlib.py
@@ -0,0 +1,13 @@
+print ("title ")
+title = input()
+print ("noun ")
+noun = input()
+print("verb")
+verb = input()
+print("adj")
+adj = input()
+print("adv")
+adv = input()
+print ("The story is about",title," :There  is a license",noun,"which can be released with date",verb, "For the above license you have to install",adj,"After this you can enjoy the software", adv,"Good luck")
+
+
